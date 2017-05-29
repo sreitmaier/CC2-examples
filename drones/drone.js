85f048400eea5e869b31f404f0ff0eb6fa217b23
Felix A. Epp <work@felixepp.de>
2017-05-30 01:30:52 +0200

    Format with Javascript Standard style

--- drones/drone.js
+++ drones/drone.js
@@ -1,86 +1,86 @@
-const DRONE_IDLE = 0;
-const DRONE_PICKUP = 1;
-const DRONE_DELIVER = 2;
+const DRONE_IDLE = 0
+const DRONE_PICKUP = 1
+const DRONE_DELIVER = 2
 
-var Drone = function(port, maxSpeed) {
-	this.rotorRadius = 10;
-	this.port = port;
-	this.position = port.copy();
-	this.target = port.copy();
-	this.speed = maxSpeed;
-	this.state = 0;
-	this.cargo = null;
+window.Drone = function (port, maxSpeed) {
+  this.rotorRadius = 10
+  this.port = port
+  this.position = port.copy()
+  this.target = port.copy()
+  this.speed = maxSpeed
+  this.state = 0
+  this.cargo = null
 
-	this.deliver = function (cargo) {
-		this.cargo = cargo;
-	};
+  this.deliver = function (cargo) {
+    this.cargo = cargo
+  }
 
-	this.stateMachine = function () {
-		switch(this.state) {
-		default:
-		case DRONE_IDLE:
-			if (this.cargo) {
-				this.target = this.cargo.position;
-				this.state = DRONE_PICKUP;
-			}
-			break;
-		case DRONE_PICKUP:
-			if (this.position.dist(this.target) < this.speed) {
-				this.target = this.cargo.target;
-				this.state = DRONE_DELIVER;
-			}
-			break;
-		case DRONE_DELIVER:
-			if (this.position.dist(this.target) < this.speed) {
-				this.target = this.port;
-				this.cargo = null;
-				this.state = DRONE_IDLE;
-			}
-			break;
-		}
-	};
+  this.stateMachine = function () {
+    switch (this.state) {
+      default:
+      case DRONE_IDLE:
+        if (this.cargo) {
+          this.target = this.cargo.position
+          this.state = DRONE_PICKUP
+        }
+        break
+      case DRONE_PICKUP:
+        if (this.position.dist(this.target) < this.speed) {
+          this.target = this.cargo.target
+          this.state = DRONE_DELIVER
+        }
+        break
+      case DRONE_DELIVER:
+        if (this.position.dist(this.target) < this.speed) {
+          this.target = this.port
+          this.cargo = null
+          this.state = DRONE_IDLE
+        }
+        break
+    }
+  }
 
-	this.availible = function () {
-		return this.state === DRONE_IDLE;
-	};
+  this.availible = function () {
+    return this.state === DRONE_IDLE
+  }
 
-	this.drive = function () {
-		var direction = p5.Vector.sub(this.target, this.position).normalize();
-		this.position.add(direction.mult(this.speed));
-	};
+  this.drive = function () {
+    var direction = p5.Vector.sub(this.target, this.position).normalize()
+    this.position.add(direction.mult(this.speed))
+  }
 
-	this.draw = function () {
-		if (this.state !== DRONE_IDLE || this.position.dist(this.target) >= this.speed) {
-			this.drive();
-		}
+  this.draw = function () {
+    if (this.state !== DRONE_IDLE || this.position.dist(this.target) >= this.speed) {
+      this.drive()
+    }
 
-		if (this.state === DRONE_PICKUP) {
-			translate(this.cargo.position.x, this.cargo.position.y);
-			this.cargo.draw();
-			translate(-this.cargo.position.x, -this.cargo.position.y);
-		}
+    if (this.state === DRONE_PICKUP) {
+      translate(this.cargo.position.x, this.cargo.position.y)
+      this.cargo.draw()
+      translate(-this.cargo.position.x, -this.cargo.position.y)
+    }
 
-		if (this.state === DRONE_DELIVER) {
-			translate(this.position.x, this.position.y);
-			this.cargo.draw();
-			translate(-this.position.x, -this.position.y);
-		}
+    if (this.state === DRONE_DELIVER) {
+      translate(this.position.x, this.position.y)
+      this.cargo.draw()
+      translate(-this.position.x, -this.position.y)
+    }
 
-		stroke(0);
-		fill(127);
-		ellipse(this.position.x-this.rotorRadius/2, this.position.y-this.rotorRadius/2, this.rotorRadius,this.rotorRadius);
-		ellipse(this.position.x+this.rotorRadius/2, this.position.y-this.rotorRadius/2, this.rotorRadius,this.rotorRadius);
-		ellipse(this.position.x-this.rotorRadius/2, this.position.y+this.rotorRadius/2, this.rotorRadius,this.rotorRadius);
-		ellipse(this.position.x+this.rotorRadius/2, this.position.y+this.rotorRadius/2, this.rotorRadius,this.rotorRadius);
+    stroke(0)
+    fill(127)
+    ellipse(this.position.x - this.rotorRadius / 2, this.position.y - this.rotorRadius / 2, this.rotorRadius, this.rotorRadius)
+    ellipse(this.position.x + this.rotorRadius / 2, this.position.y - this.rotorRadius / 2, this.rotorRadius, this.rotorRadius)
+    ellipse(this.position.x - this.rotorRadius / 2, this.position.y + this.rotorRadius / 2, this.rotorRadius, this.rotorRadius)
+    ellipse(this.position.x + this.rotorRadius / 2, this.position.y + this.rotorRadius / 2, this.rotorRadius, this.rotorRadius)
 
-		if (this.state === DRONE_IDLE) {
-			fill('#00ff00');
-		} else {
-			fill('#ff0000');
-		}
-		noStroke();
-		ellipse(this.position.x, this.position.y, this.rotorRadius/2,this.rotorRadius/2);
+    if (this.state === DRONE_IDLE) {
+      fill('#00ff00')
+    } else {
+      fill('#ff0000')
+    }
+    noStroke()
+    ellipse(this.position.x, this.position.y, this.rotorRadius / 2, this.rotorRadius / 2)
 
-		this.stateMachine();
-	};
-};
+    this.stateMachine()
+  }
+}
--- drones/sketch.js
+++ drones/sketch.js
@@ -1,72 +1,72 @@
-var vehicles = [];
-var jobs = [];
-var newJob;
-var jobState = 0;
-var vehicleCount = 6;
+var vehicles = []
+var jobs = []
+var newJob
+var jobState = 0
+var vehicleCount = 6
 
-function setup() {
-	createCanvas(windowWidth, windowHeight);
-	rectMode(CENTER);
+function setup () {
+  createCanvas(windowWidth, windowHeight)
+  rectMode(CENTER)
 
-	for (var i = 0; i < vehicleCount; i++) {
-		vehicles[i] = new Drone(createVector(windowWidth/4*(1+2*(i%2)), windowHeight/2), 2);
-	}
+  for (var i = 0; i < vehicleCount; i++) {
+    vehicles[i] = new Drone(createVector(windowWidth / 4 * (1 + 2 * (i % 2)), windowHeight / 2), 2)
+  }
 
-	newJob = new Package();
+  newJob = new Package()
 }
 
-function draw() {
-	handleJobs();
+function draw () {
+  handleJobs()
 
-	background(255);
+  background(255)
 
-	for (var vehicle of vehicles) {
-		vehicle.draw();
-	}
+  for (var vehicle of vehicles) {
+    vehicle.draw()
+  }
 
-	if (jobState) {
-		stroke(85);
-		line(newJob.position.x, newJob.position.y, mouseX, mouseY);
+  if (jobState) {
+    stroke(85)
+    line(newJob.position.x, newJob.position.y, mouseX, mouseY)
 
-		translate(newJob.position.x, newJob.position.y);
-		newJob.draw();
-		translate(-newJob.position.x, -newJob.position.y);
+    translate(newJob.position.x, newJob.position.y)
+    newJob.draw()
+    translate(-newJob.position.x, -newJob.position.y)
 
-		stroke(0);
-		noFill();
-		ellipse(mouseX, mouseY, 15, 15);
-		line(mouseX-10, mouseY, mouseX+10, mouseY);
-		line(mouseX, mouseY-10, mouseX, mouseY+10);
-	} else {
-		translate(mouseX, mouseY);
-		newJob.draw();
-		translate(-mouseX, -mouseY);
-	}
+    stroke(0)
+    noFill()
+    ellipse(mouseX, mouseY, 15, 15)
+    line(mouseX - 10, mouseY, mouseX + 10, mouseY)
+    line(mouseX, mouseY - 10, mouseX, mouseY + 10)
+  } else {
+    translate(mouseX, mouseY)
+    newJob.draw()
+    translate(-mouseX, -mouseY)
+  }
 }
 
-function handleJobs() {
-	while (jobs.length) {
-		var availableVehicles = vehicles.filter((vehicle) => vehicle.availible());
-		if (availableVehicles.length) {
-			var job = jobs.pop();
-			availableVehicles.reduce((closestVehicle, vehicle) => (!closestVehicle || closestVehicle.position.dist(job.position) > vehicle.position.dist(job.position)) ? vehicle : closestVehicle).deliver(job);
-		} else {
-			return;
-		}
-	}
+function handleJobs () {
+  while (jobs.length) {
+    var availableVehicles = vehicles.filter((vehicle) => vehicle.availible())
+    if (availableVehicles.length) {
+      var job = jobs.pop()
+      availableVehicles.reduce((closestVehicle, vehicle) => (!closestVehicle || closestVehicle.position.dist(job.position) > vehicle.position.dist(job.position)) ? vehicle : closestVehicle).deliver(job)
+    } else {
+      return
+    }
+  }
 }
 
-function mouseClicked() {
-	switch (jobState) {
-	case 0: //click new package
-		newJob.position = createVector(mouseX, mouseY);
-		jobState = 1;
-		break;
-	case 1:
-		newJob.target = createVector(mouseX, mouseY);
-		jobs.push(newJob);
-		jobState = 0;
-		newJob = new Package();
-		break;
-	}
+function mouseClicked () {
+  switch (jobState) {
+    case 0: // click new package
+      newJob.position = createVector(mouseX, mouseY)
+      jobState = 1
+      break
+    case 1:
+      newJob.target = createVector(mouseX, mouseY)
+      jobs.push(newJob)
+      jobState = 0
+      newJob = new Package()
+      break
+  }
 }
